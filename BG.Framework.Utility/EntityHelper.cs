07a9ef40198aa3fc1d939c06e05489b337bf5652
BoGeManger <664120843@qq.com>
2018-04-05 18:10:52 +0800

    20180405

---
+++ BG.Framework.Utility/EntityHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BG.Framework.Utility
+{
+    public class EntityHelper
+    {
+        /// <summary>
+        ///用","号分隔忽略属性
+        /// </summary>
+        /// <param name="source">源对象</param>
+        /// <param name="target">目标对象</param>
+        /// <param name="ignorePoperties"></param>
+        /// <returns></returns>
+        public static TTarget EntityCopy<TSource, TTarget>(TSource source, TTarget target, string ignorePoperties)
+        {
+            List<string> ignoreP = new List<string>();
+            if (!string.IsNullOrEmpty(ignorePoperties))
+            {
+                ignoreP = ignorePoperties.ToLower().Split(',').ToList();
+            }
+            var tFields = target.GetType().GetProperties();
+            var sFields = source.GetType().GetProperties();
+
+            foreach (var item in tFields)
+            {
+                if (!ignoreP.Contains(item.Name.ToLower()))
+                {
+                    foreach (var si in sFields)
+                    {
+                        if (si.Name == item.Name)
+                        {
+                            object svalue = si.GetValue(source, null);
+                            object tvalue = item.GetValue(target, null);
+                            if (svalue != null && !svalue.Equals(tvalue))
+                            {
+                                item.SetValue(target, svalue, null);
+                            }
+                        }
+                    }
+                }
+            }
+            return target;
+        }
+
+
+
+        /// <summary>
+        ///用","号分隔忽略属性
+        /// </summary>
+        /// <param name="source">源对象</param>
+        /// <param name="target">目标对象</param>
+        /// <returns></returns>
+        public static TTarget EntityCopy<TSource, TTarget>(TSource source, TTarget target)
+        {
+            return EntityCopy(source, target, "");
+
+        }
+
+    }
+}
---
+++ BG.Framework.Utility/EnumHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace BG.Framework.Utility
+{
+    /// <summary>
+    /// 枚举帮助类
+    /// </summary>
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// 尝试根据字符串转换枚举类型，默认忽略大小写
+        /// </summary>
+        /// <typeparam name="TEnum">枚举类型</typeparam>
+        /// <param name="value">字符串值</param>
+        /// <param name="result">结果</param>
+        /// <returns>转换成功返回true</returns>
+        public static bool TryParse<TEnum>(string value, out TEnum result)
+        {
+            return TryParse<TEnum>(value, true, out result);
+        }
+
+        /// <summary>
+        /// 尝试根据字符串转换枚举类型
+        /// </summary>
+        /// <typeparam name="TEnum">枚举类型</typeparam>
+        /// <param name="value">字符串值</param>
+        /// <param name="ignoreCase">是否忽略大小写</param>
+        /// <param name="result">结果</param>
+        /// <returns>转换成功返回true</returns>
+        public static bool TryParse<TEnum>(string value, bool ignoreCase, out TEnum result)
+        {
+            bool flag;
+
+            try
+            {
+                result = (TEnum)Enum.Parse(typeof(TEnum), value, ignoreCase);
+                flag = true;
+            }
+            catch
+            {
+                result = default(TEnum);
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        /// <summary>
+        /// 扩展方法，获得枚举的Description
+        /// </summary>
+        /// <param name="value">枚举值</param>
+        /// <param name="nameInstead">当枚举值没有定义DescriptionAttribute，是否使用枚举名代替，默认是使用</param>
+        /// <returns>枚举的Description</returns>
+        public static string GetDescription(this Enum value, Boolean nameInstead = true)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name == null)
+            {
+                return "";
+            }
+            FieldInfo field = type.GetField(name);
+            object[] attr = field.GetCustomAttributes(nameInstead);
+            if (attr.Length > 0)
+                return (attr[0] as DescriptionAttribute).Description;
+            return "";
+        }
+    }
+}
